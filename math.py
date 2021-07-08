41391adbcf0a9b53d2328ee6c90f8bd9eca6551c
gayatrighanekar <87116015+gayatrighanekar@users.noreply.github.com>
2021-07-08 14:28:45 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#add addition
+def add(x,y):
+    return x+y
+
+#add subtract
+def subtract(x,y):
+    return x-y   #on master 
+
+#add multiplication
+def multiply(x,y):
+    return x*y #multiply 
+
+#add division
+def divide(x,y):
+   if y==0:
+	return divide_error
+   else:
+	return x/y
+
